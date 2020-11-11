7f676380b24baec663a7f951b316e12a827654bc
Tauan Marinho <tauamarinho@gmail.com>
2020-11-11 11:19:13 -0300

    Refactor

--- src/main/java/com/podium/test/tests/EnterprisePageTest.java
+++ src/main/java/com/podium/test/tests/EnterprisePageTest.java
@@ -3,18 +3,31 @@ package com.podium.test.tests;
 import com.podium.test.UiBaseTest;
 import com.podium.test.components.EnterprisePage;
 import com.podium.test.components.Podium;
+import com.podium.test.driver.SetupDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class EnterprisePageTest extends UiBaseTest {
 
     private EnterprisePage enterprisePage;
+    private static WebDriver webDriver;
+    protected Podium podium;
 
     @BeforeClass
     private void navigateEnterprise() {
+        System.out.println("Test Case One with Thread Id:- "
+                + Thread.currentThread().getId());
+        this.webDriver = loadWebDriver();
         enterprisePage = new Podium(webDriver)
             .navigateTo(BASE_URL)
             .clickEnterpriseButton();
+    }
+
+    @AfterClass
+    private void quit() {
+        quitWebDriver(webDriver);
     }
 
     @Test(priority = 1, description = "Validate all main fields Enterprise Page")
--- src/main/java/com/podium/test/tests/LoginPageTest.java
+++ src/main/java/com/podium/test/tests/LoginPageTest.java
@@ -5,15 +5,27 @@ import com.podium.test.components.Login;
 import com.podium.test.components.Podium;
 import com.podium.test.data.DataDriven;
 import com.podium.test.data.DataDrivenProvider;
+import com.podium.test.driver.SetupDriver;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
 public class LoginPageTest extends UiBaseTest {
 
     private Login podiumLoginPage;
+    private static WebDriver webDriver;
+    protected Podium podium;
 
     @BeforeClass
     private void navigateLogin() {
+        System.out.println("Test Case One with Thread Id:- "
+                + Thread.currentThread().getId());
+        this.webDriver = loadWebDriver();
         podiumLoginPage = new Podium(webDriver).navigateTo(BASE_URL).clickLogin();
+    }
+
+    @AfterClass
+    private void quit() {
+        quitWebDriver(webDriver);
     }
 
     @Test(priority = 1, description = "Validate all main fields Login Page")
